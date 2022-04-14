544570a21ebcda44d9d22bb64fca9669c77876a3
Thamires <thamires_lcarvalho@hotmail.com>
2022-04-13 22:01:21 -0300

    atividade calculadora

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+var funcSomar = require('./somar')
+var funcSubtrair = require('./subtrair')
+var funcDividir = require('./dividir')
+var funcMultiplicar = require('./multiplicar')
+
+console.log(funcDividir(400, 2))
+
+console.log(funcMultiplicar(5, 40))
+
+console.log(funcSomar(100, 100))
+
+console.log(funcSubtrair(300, 100))
